7d3ed1f118d79d1a093d11a2f88a39d1bd405f1f
Gavin.Guo <gavin.guo@powervision.me>
2018-01-24 17:36:59 +0800

    Commit init.

---
+++ iOS_Tools/iOS_Tools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Tools
+//
+//  Created by Gavin.Guo on 2018/1/24.
+//  Copyright © 2018年 GSNICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Tools/iOS_Tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_Tools
+//
+//  Created by Gavin.Guo on 2018/1/24.
+//  Copyright © 2018年 GSNICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
